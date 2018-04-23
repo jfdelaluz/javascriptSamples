5334a7d0cd1c7185ff9a8c83539a452506c983a2
jfdelaluz <jfdelaluz@gmail>
2018-04-23 12:39:23 -0700

    added pseudoclassical samples

--- js/inheritance-samples/inheritance-samples.js
+++ js/inheritance-samples/inheritance-samples.js
@@ -1,9 +1,12 @@
 var InheritanceSamples = (function() {
   var self = {},
-      Mammal;
+      Mammal,
+      Cat,
+      Dog;
   
   self.init = function() {
     augmentFunctionMethodNew();
+    augmentFunctionMethodInherits();
     createMammalObject();
   };
   
@@ -14,6 +17,13 @@ var InheritanceSamples = (function() {
       var other = this.apply(that, arguments);
       
       return (typeof other === 'object' && other) || that;
+    });
+  }
+
+  function augmentFunctionMethodInherits() {
+    Function.method('inherits', function (Parent) {
+      this.prototype = new Parent();
+      return this;
     });
   }
   
@@ -37,6 +47,68 @@ var InheritanceSamples = (function() {
     var myMammal = new Mammal('Herb the Mammal');
     var name = myMammal.get_name();
     BaseModule.Printer.printLine('New instance property: var name = myMammal.get_name(); = ' + name);
+
+    BaseModule.Printer.printLine('Create "Cat" pseudoclass that inherits from "Mammal"');
+    Cat = function (name) {
+      this.name = name;
+      this.saying = 'meow';
+    };
+
+    Cat.prototype = new Mammal();
+
+    Cat.prototype.purr = function (n) {
+      var i, s = '';
+      for (i = 0; i < n; i++) {
+        if (s) {
+          s += '-';
+        }
+        s += 'r';
+      }
+      return s;
+    };
+
+    Cat.prototype.get_name = function () {
+      return this.says() + ' ' + this.name + ' ' + this.says();
+    };
+
+    BaseModule.Printer.printLine('Create instance from "Cat": var myCat = new Cat(\'Henrietta\');');
+    var myCat = new Cat('Henrietta');
+    BaseModule.Printer.printLine('Execute myCat "says" method: myCat.says() = ' + myCat.says());
+    BaseModule.Printer.printLine('Execute myCat "purr" method: myCat.purr(5) = ' + myCat.purr(5));
+    BaseModule.Printer.printLine('"Cat" pseudoclass "purr" method exists: typeof myCat.purr = ' + typeof myCat.purr);
+    BaseModule.Printer.printLine('"Mammal" does not have "purr" method: typeof myMammal.purr = ' + typeof myMammal.purr);
+    BaseModule.Printer.printLine(
+      'New instance created "myCat" has access to "get_name" method: myCat.get_name() = ' + myCat.get_name()
+    );
+
+    BaseModule.Printer.printLine('Create "Dog" pseudoclass with augmented "inherits" method on "Function" object');
+    Dog = function (name) {
+      this.name = name;
+      this.saying = 'arf';
+    }
+    .inherits(Mammal)
+    .method('run', function (n) {
+      var i, s = '';
+      for (i = 0; i < n; i++) {
+        if (s) {
+          s += '-';
+        }
+        s += 'running';
+      }
+      return s;
+    })
+    .method('get_name', function () {
+      return this.says() + ' ' + this.name + ' ' + this.says();
+    });
+
+    var myDog = new Dog('Spunky');
+    BaseModule.Printer.printLine('Execute myDog "says" method: myDog.says() = ' + myDog.says());
+    BaseModule.Printer.printLine('Execute myDog "run" method: myDog.run(3) = ' + myDog.run(3));
+    BaseModule.Printer.printLine('"Dog" pseudoclass "run" method exists: typeof myDog.run = ' + typeof myDog.run);
+    BaseModule.Printer.printLine('"Mammal" does not have "run" method: typeof myMammal.run = ' + typeof myMammal.run);
+    BaseModule.Printer.printLine(
+      'New instance created "myDog" has access to "get_name" method: myDog.get_name() = ' + myDog.get_name()
+    );
     
     BaseModule.Printer.sectionSeparator();
   }
